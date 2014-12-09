3636e70199e20bb27cacdbfde944518d8df019f6
Simon Balz <sba@lcsys.ch>
2014-12-09 19:14:25 +0100

    Added new script for alert settings

---
+++ appserver/static/alert_settings.js
@@ -0,0 +1,28 @@
+require([
+    "splunkjs/mvc",
+    "splunkjs/mvc/utils",
+    "splunkjs/mvc/tokenutils",
+    "underscore",
+    "jquery",
+    "splunkjs/mvc/simplexml",
+    'splunkjs/mvc/tableview',
+    'splunkjs/mvc/chartview',
+    'splunkjs/mvc/searchmanager'   
+], function(
+        mvc,
+        utils,
+        TokenUtils,
+        _,
+        $,
+        DashboardController,
+        TableView,
+        ChartView,
+        SearchManager 
+    ) {
+
+    // Tokens
+    var submittedTokens = mvc.Components.getInstance('submitted', {create: true});
+    var defaultTokens   = mvc.Components.getInstance('default', {create: true});
+
+    console.debug("loaded");
+});
