a0651e42053a8615dee208344cf97867c3bb5684
Simon Balz <simon@balz.me>
2015-03-30 20:59:17 +0200

    Changed dataload to support multiple datasets

--- bin/initial_dataload.sh
+++ bin/initial_dataload.sh
@@ -11,10 +11,10 @@ SPLUNK_PASS=changeme
 # Purge notification schemes
 curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} -X DELETE ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/notification_schemes
 # Load default notification scheme
-curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/notification_schemes -H 'Content-Type: application/json' -d @${SPLUNK_HOME}/etc/apps/alert_manager/appserver/src/default_notification_scheme.json
+curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/notification_schemes/batch_save -H 'Content-Type: application/json' -d @${SPLUNK_HOME}/etc/apps/alert_manager/appserver/src/default_notification_scheme.json
 
 
 # Purge email templates
 curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} -X DELETE ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/email_templates
 # Load default email templates
-curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/email_templates -H 'Content-Type: application/json' -d @${SPLUNK_HOME}/etc/apps/alert_manager/appserver/src/default_email_templates.json
+curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/email_templates/batch_save -H 'Content-Type: application/json' -d @${SPLUNK_HOME}/etc/apps/alert_manager/appserver/src/default_email_templates.json
