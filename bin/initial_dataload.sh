eab37dc089e5c1442fd903853ae8f7374472e5d5
Simon Balz <simon@balz.me>
2015-04-01 10:15:17 +0200

    Updated sample data

--- bin/initial_dataload.sh
+++ bin/initial_dataload.sh
@@ -12,7 +12,7 @@ SPLUNK_PASS=changeme
 curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} -X DELETE ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/notification_schemes
 # Load default notification scheme
 curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/notification_schemes/batch_save -H 'Content-Type: application/json' -d @${SPLUNK_HOME}/etc/apps/alert_manager/appserver/src/default_notification_scheme.json
-
+curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/notification_schemes/batch_save -H 'Content-Type: application/json' -d @${SPLUNK_HOME}/etc/apps/alert_manager/appserver/src/custom_notification_scheme.json
 
 # Purge email templates
 curl -k -u ${SPLUNK_USER}:${SPLUNK_PASS} -X DELETE ${SPLUNK_HOST}/servicesNS/nobody/alert_manager/storage/collections/data/email_templates
