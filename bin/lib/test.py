1c35e3d7dcca3265a12c33e170ac16f28cd9168b
Simon Balz <simon@balz.me>
2015-04-01 15:14:56 +0200

    Removed test scripts

--- bin/lib/test.py
+++
@@ -1,4 +0,0 @@
-from CsvResultParser import *
-
-parser = CsvResultParser('/opt/splunk/var/run/splunk/dispatch/scheduler__admin_U0EtYWxlcnRfbWFuYWdlcl9kZW1v__RMD5ad7b61a9b44b3088_at_1422814680_605/results.csv.gz')
-print parser.getResults()
