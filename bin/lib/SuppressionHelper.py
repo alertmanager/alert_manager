ce8afd2e9b62d29914e735b91ca850e4821c611f
Simon Balz <simon@balz.me>
2015-10-21 10:49:37 +0200

    Minor cleanup stuff

--- bin/lib/SuppressionHelper.py
+++ bin/lib/SuppressionHelper.py
@@ -27,7 +27,7 @@ class SuppressionHelper:
     formatter = logging.Formatter("%(asctime)-15s %(levelname)-5s %(message)s")
     fh.setFormatter(formatter)
     log.addHandler(fh)
-    log.setLevel(logging.DEBUG)
+    log.setLevel(logging.INFO)
 
     sessionKey  = None
 
