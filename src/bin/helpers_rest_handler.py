312aaceb41a5e8389efd3a4fba77de7948971395
my2ndhead <johndoe@example.com>
2018-06-25 15:57:43 +0200

    Small fixes

--- src/bin/helpers_rest_handler.py
+++ src/bin/helpers_rest_handler.py
@@ -369,6 +369,35 @@ class HelpersHandler(PersistentServerConnectionApplication):
     def _create_new_incident(self, sessionKey, user, post_data):
         logger.debug("START _create_new_incident()")
 
+        config = {}
+        config['index'] = 'main'
+        config['collect_data_results'] = False
+        config['index_data_results'] = False
+
+        # Get config data
+        restconfig = entity.getEntities('configs/alert_manager', count=-1, sessionKey=sessionKey)
+        if len(restconfig) > 0:
+            # Get index
+            if 'index' in restconfig['settings']:
+                config['index'] = restconfig['settings']['index']
+
+            # Check if results have be written to collection
+            if 'collect_data_results' in restconfig['settings']:
+                if restconfig['settings']['collect_data_results'].lower() in ('1', 'true'):
+                    config['collect_data_results'] = True
+                else:
+                    config['collect_data_results'] = False    
+
+            # Check if results have be indexed
+            if 'index_data_results' in restconfig['settings']:
+                if restconfig['settings']['index_data_results'].lower() in ('1', 'true'):
+                    config['index_data_results'] = True
+                else:
+                    config['index_data_results'] = False
+
+        logger.info("Global settings: %s" % config)
+
+        # Create timestamp for event
         gmtime = time.gmtime()
         now = time.strftime("%Y-%m-%dT%H:%M:%S.000%z", gmtime)
         now_epoch = time.strftime("%s", gmtime)
@@ -400,7 +429,7 @@ class HelpersHandler(PersistentServerConnectionApplication):
         if not event_search:
             event_search = '|noop'
         if not earliest_time:
-            earliest_time = now
+            earliest_time = int(now_epoch)-1
         if not latest_time:
             latest_time = now    
 
@@ -418,13 +447,18 @@ class HelpersHandler(PersistentServerConnectionApplication):
                 logger.exception(msg)
                 return self.response(msg, httplib.INTERNAL_SERVER_ERROR)
 
+        # Create unique id
+        incident_id = str(uuid.uuid4())
+
+        # Create event_id
+        event_id = hashlib.md5(incident_id + now).hexdigest()
 
         # Defaults
         ttl                   = 3600        
         alert_time            = now
         search_name           = 'Manual Alert'
         result_id             = 0
-        job_id                = ''
+        job_id                = event_id
         alert                 = title
         display_fields        = ''
         external_reference_id = ''
@@ -432,24 +466,9 @@ class HelpersHandler(PersistentServerConnectionApplication):
         status                = 'new'
         app                   = 'alert_manager'
 
-        # Create unique id
-        incident_id = str(uuid.uuid4())
-
-        # Get Index
-    	config = {}
-        config['index'] = 'main'
-
-        restconfig = entity.getEntities('configs/alert_manager', count=-1, sessionKey=sessionKey)
-        if len(restconfig) > 0:
-            if 'index' in restconfig['settings']:
-                config['index'] = restconfig['settings']['index']
-
-        event_id = hashlib.md5(incident_id + now).hexdigest()
-
         # Create metadata event
         metadata = '{"alert":"%s", "alert_time": "%s", "origin": "%s", "app": "%s", "category": "%s", "display_fields":  "%s", "entry":[{"content": {"earliestTime": "%s", "eventSearch": "%s","latestTime": "%s"}}], "external_reference_id": "%s", "impact": "%s", "incident_id": "%s", "job_id": "%s", "owner": "%s", "priority": "%s", "result_id": "%s", "subcategory": "%s", "tags": "%s", "title": "%s", "ttl": "%s", "urgency": "%s"}' % (alert, now, origin, app, category, display_fields, earliest_time, event_search, latest_time, external_reference_id, impact, incident_id, job_id, owner, priority, result_id, subcategory, tags, title, ttl, urgency)
-
-        logger.info("Metadata %s" % metadata)
+        logger.debug("Metadata %s" % metadata)
 
         try:
             splunk.setDefault('sessionKey', sessionKey)
@@ -483,7 +502,7 @@ class HelpersHandler(PersistentServerConnectionApplication):
         entry['external_reference_id'] = external_reference_id
 
         entry = json.dumps(entry, sort_keys=True)
-        logger.info("createIncident(): Entry: %s" % entry)
+        logger.debug("createIncident(): Entry: %s" % entry)
         
         uri = '/servicesNS/nobody/alert_manager/storage/collections/data/incidents'
         rest.simpleRequest(uri, sessionKey=sessionKey, jsonargs=entry)
@@ -499,28 +518,44 @@ class HelpersHandler(PersistentServerConnectionApplication):
             for key in fields:
                 field_list.append(key)
 
-            logger.info("fields: %s" % fields)    
+            logger.debug("fields: %s" % fields)    
 
             results = {}
             results['incident_id'] = incident_id
-            results['job_id'] = job_id
-            results['result_id'] = result_id
             results['fields'] = field_array
             results['field_list'] = field_list
 
-            results = json.dumps(results, sort_keys=True)
+            logger.debug("Entry: %s" % results)
 
-            logger.info("Entry: %s" % results)
+            # Write results to incident_results collection
+            if config['collect_data_results'] == True:
+                try:
+                    # Add job_id and result_id to collection
+                    results['job_id'] = job_id
+                    results['result_id'] = result_id
+                    results = json.dumps(results, sort_keys=True)
 
-            try:
-                uri = '/servicesNS/nobody/alert_manager/storage/collections/data/incident_results'
-                serverResponse, serverContent = rest.simpleRequest(uri, sessionKey=sessionKey, jsonargs=results)
-                logger.info("Results for incident_id=%s written to collection." % (incident_id))
+                    uri = '/servicesNS/nobody/alert_manager/storage/collections/data/incident_results'
+                    rest.simpleRequest(uri, sessionKey=sessionKey, jsonargs=results)
+                    logger.info("Results for incident_id=%s written to collection." % (incident_id))
 
-            except:
-                msg = 'Unhandled Exception: {}'.format(str(e))
-                logger.exception(msg)
-                return self.response(msg, httplib.INTERNAL_SERVER_ERROR)
+                except:
+                    msg = 'Unhandled Exception: {}'.format(str(e))
+                    logger.exception(msg)
+                    return self.response(msg, httplib.INTERNAL_SERVER_ERROR)
+
+            # Write results to index
+            if config['index_data_results'] == True:
+                try:
+                    results = json.dumps(results, sort_keys=True)
+
+                    input.submit(results, hostname = socket.gethostname(), sourcetype = 'alert_data_results', source = 'helper.py', index = config['index'])
+                    logger.info("Results for incident_id=%s written to index." % (incident_id))
+
+                except:
+                    msg = 'Unhandled Exception: {}'.format(str(e))
+                    logger.exception(msg)
+                    return self.response(msg, httplib.INTERNAL_SERVER_ERROR)
 
         # Create incident_change events
         event = 'time=%s event_id=%s severity=INFO origin="alert_handler" user="%s" action="create" alert="%s" incident_id="%s" job_id="%s" result_id="%s" owner="%s" status="new" urgency="%s" ttl="%s" alert_time="%s"' % (now, event_id, user, search_name, incident_id, job_id, result_id, owner, urgency, ttl, alert_time)
