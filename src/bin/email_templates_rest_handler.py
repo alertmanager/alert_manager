10a1ac6927370d38e9073f1eccf93f9887ce52e2
my2ndhead <mika.borner@gmail.com>
2020-04-26 20:44:33 +0200

    Porting to Python 3.7

--- src/bin/email_templates_rest_handler.py
+++ src/bin/email_templates_rest_handler.py
@@ -1,13 +1,12 @@
 import os
 import sys
-import urllib
 import json
 import re
 import datetime
-import urllib
+import urllib.parse
 import hashlib
 import socket
-import httplib
+import http.client
 import operator
 from string import Template
 
@@ -53,16 +52,16 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
             if callable(getattr(self, method, None)):
                 return operator.methodcaller(method, args)(self)
             else:
-                return self.response('Invalid method for this endpoint', httplib.METHOD_NOT_ALLOWED)
+                return self.response('Invalid method for this endpoint', http.client.METHOD_NOT_ALLOWED)
         except ValueError as e:
             msg = 'ValueError: {}'.format(e.message)
-            return self.response(msg, httplib.BAD_REQUEST)
+            return self.response(msg, http.client.BAD_REQUEST)
         except splunk.RESTException as e:
-            return self.response('RESTexception: {}'.format(e), httplib.INTERNAL_SERVER_ERROR)
+            return self.response('RESTexception: {}'.format(e), http.client.INTERNAL_SERVER_ERROR)
         except Exception as e:
             msg = 'Unknown exception: {}'.format(e)
             logger.exception(msg)
-            return self.response(msg, httplib.INTERNAL_SERVER_ERROR)
+            return self.response(msg, http.client.INTERNAL_SERVER_ERROR)
 
 
     def handle_get(self, args):
@@ -74,13 +73,13 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
             sessionKey = args["session"]["authtoken"]
             user = args["session"]["user"]
         except KeyError:
-            return self.response("Failed to obtain auth token", httplib.UNAUTHORIZED)
+            return self.response("Failed to obtain auth token", http.client.UNAUTHORIZED)
 
 
         required = ['action']
         missing = [r for r in required if r not in query_params]
         if missing:
-            return self.response("Missing required arguments: {}".format(missing), httplib.BAD_REQUEST)
+            return self.response("Missing required arguments: {}".format(missing), http.client.BAD_REQUEST)
 
         action = '_' + query_params.pop('action').lower()
         if callable(getattr(self, action, None)):
@@ -88,7 +87,7 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
         else:
             msg = 'Invalid action: action="{}"'.format(action)
             logger.exception(msg)
-            return self.response(msg, httplib.BAD_REQUEST)
+            return self.response(msg, http.client.BAD_REQUEST)
 
     def handle_post(self, args):
         logger.debug('POST ARGS {}'.format(json.dumps(args)))
@@ -99,13 +98,13 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
             sessionKey = args["session"]["authtoken"]
             user = args["session"]["user"]
         except KeyError:
-            return self.response("Failed to obtain auth token", httplib.UNAUTHORIZED)
+            return self.response("Failed to obtain auth token", http.client.UNAUTHORIZED)
 
 
         required = ['action']
         missing = [r for r in required if r not in post_data]
         if missing:
-            return self.response("Missing required arguments: {}".format(missing), httplib.BAD_REQUEST)
+            return self.response("Missing required arguments: {}".format(missing), http.client.BAD_REQUEST)
 
         action = '_' + post_data.pop('action').lower()
         if callable(getattr(self, action, None)):
@@ -113,7 +112,7 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
         else:
             msg = 'Invalid action: action="{}"'.format(action)
             logger.exception(msg)
-            return self.response(msg, httplib.BAD_REQUEST)
+            return self.response(msg, http.client.BAD_REQUEST)
 
 
     @staticmethod
@@ -136,19 +135,19 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
         required = ['key']
         missing = [r for r in required if r not in post_data]
         if missing:
-            return self.response("Missing required arguments: {}".format(missing), httplib.BAD_REQUEST)
+            return self.response("Missing required arguments: {}".format(missing), http.client.BAD_REQUEST)
 
         key = post_data.pop('key')
 
         query = {}
         query['_key'] = key
-        logger.debug("Query for email templates: {}".format(urllib.quote(json.dumps(query))))
-        uri = '/servicesNS/nobody/alert_manager/storage/collections/data/email_templates?query={}'.format(urllib.quote(json.dumps(query)))
+        logger.debug("Query for email templates: {}".format(urllib.parse.quote(json.dumps(query))))
+        uri = '/servicesNS/nobody/alert_manager/storage/collections/data/email_templates?query={}'.format(urllib.parse.quote(json.dumps(query)))
         serverResponse, serverContent = rest.simpleRequest(uri, sessionKey=sessionKey, method='DELETE')
 
         logger.debug("Email Template removed. serverResponse was {}".format(serverResponse))
 
-        return self.response('Email Template with key {} successfully removed'.format(key), httplib.OK)
+        return self.response('Email Template with key {} successfully removed'.format(key), http.client.OK)
 
     def _update_email_templates(self, sessionKey, user, post_data):
         logger.debug("START _update_email_templates()")
@@ -156,7 +155,7 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
         required = ['template_data']
         missing = [r for r in required if r not in post_data]
         if missing:
-            return self.response("Missing required arguments: {}".format(missing), httplib.BAD_REQUEST)
+            return self.response("Missing required arguments: {}".format(missing), http.client.BAD_REQUEST)
 
         template_data = post_data.pop('template_data')
 
@@ -187,7 +186,7 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
                 serverResponse, serverContent = rest.simpleRequest(uri, sessionKey=sessionKey, jsonargs=entry)
                 logger.debug("Added entry. serverResponse was {}".format(serverResponse))
 
-        return self.response('Email Templates successfully updated', httplib.OK)
+        return self.response('Email Templates successfully updated', http.client.OK)
 
     def _get_email_template_files(self, sessionKey, query_params):
         logger.debug("START _get_email_template_files()")
@@ -208,4 +207,4 @@ class EmailTemplatesHandler(PersistentServerConnectionApplication):
                     if f not in file_list:
                         file_list.append(f)
 
-        return self.response(file_list, httplib.OK)
+        return self.response(file_list, http.client.OK)
